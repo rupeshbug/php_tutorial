1aa04b025593fd30e803ac2bff7b782f64a7150e
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-01-18 16:59:45 +0545

    variables

---
+++ variables.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "<h1>Variables</h1> <br>";
+
+$name = "Rupesh Chaulagain";
+$email = "fake@gmail.com";
+$food = "pizza";
+
+$age = 21;
+$quantity = 3;
+
+$gpa = 3.8;
+
+$online = true;
+
+echo "Hello {$name} <br>";
+echo "Your email is {$email} <br>";
+
+echo "You are {$age} years old <br>";
+
+echo "Your gpa is: {$gpa} <br>";
+
+echo "Online status: {$online} <br>";
