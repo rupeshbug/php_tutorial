a63219cd031e1e8e86caa5e5d1770231e5540795
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-01-18 16:43:50 +0545

    minor changes

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-echo "Its php tutorial <br>";
+echo "I love pizza <br>";
 echo "Its really good";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <br>
+    <button>Order a pizza</button>
+</body>
+
+</html>
