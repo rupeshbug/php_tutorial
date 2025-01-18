7012aab0e312cf8454a4c449cad2dbf009fa149f
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-01-18 17:35:25 +0545

    get and post request practise

--- index.php
+++ index.php
@@ -9,19 +9,22 @@
 
 <body>
     <form action="index.php" method="post">
-        <label>username: </label><br>
-        <input type=" text" name="username"><br>
-
-        <label>password: </label><br>
-        <input type="password" name="password"><br>
-
-        <input type="submit" value="Log in">
+        <label>quantity: </label><br>
+        <input type=" text" name="quantity"><br>
+        <input type="submit" value="total">
     </form>
 </body>
 
 </html>
 
 <?php
-echo "{$_POST["username"]} <br>";
-echo "{$_POST["password"]} <br>";
+$item = "pizza";
+$price = 5.99;
+$quantity = $_POST["quantity"];
+$total = null;
+
+$total = $quantity * $price;
+
+echo "You have ordered {$quantity} x {$item}/s <br>";
+echo "Your total is: \${$total}";
 ?>
