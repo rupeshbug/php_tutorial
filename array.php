cbf160e662450bbcbc74cffa37c2dedf6848b3aa
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-01-20 16:10:29 +0545

    associative array

--- array.php
+++ array.php
@@ -1,38 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="array.php" method="post">
+        <label>Enter a country name: </label><br>
+        <input type=" text" name="country"><br>
+        <input type="submit" value="submit">
+    </form>
+</body>
+
+</html>
+
+
 <?php
-$foods = array("apple", "orange", "banana");
+// $foods = array("apple", "orange", "banana");
 
 // echo $foods[0] . "<br>";
 // echo $foods[1] . "<br>";
 // echo $foods[2] . "<br>";
 
 // $foods[0] = "grapes";
-array_push($foods, "grapes", "kiwi");
+// array_push($foods, "grapes", "kiwi");
 
-array_pop($foods);
+// array_pop($foods);
 
-$reversed_foods = array_reverse($foods);
+// $reversed_foods = array_reverse($foods);
 
-foreach ($foods as $food) {
-    echo "{$food} <br>";
-}
+// foreach ($foods as $food) {
+//     echo "{$food} <br>";
+// }
 
-foreach ($reversed_foods as $food) {
-    echo "{$food} <br>";
-}
+// foreach ($reversed_foods as $food) {
+//     echo "{$food} <br>";
+// }
 
-echo "<br>";
-echo  "<br>";
+// echo "<br>";
+// echo  "<br>";
 
-// assiciative array
+// ********** assiciative array ***********
 
 $capitals = array("Nepal" => "Kathmandu", "USA" => "Washington D.C", "Japan" => "Tokyo");
 
-foreach ($capitals as $key => $value) {
-    echo "{$key} ===> {$value} <br>";
-}
+// foreach ($capitals as $key => $value) {
+//     echo "{$key} ===> {$value} <br>";
+// }
 
-$keys = array_keys($capitals);
+// $keys = array_keys($capitals);
 
-foreach ($keys as $key) {
-    echo "{$key} <br>";
-}
+// foreach ($keys as $key) {
+//     echo "{$key} <br>";
+// }
+
+$country = $_POST["country"];
+$capital = $capitals[$_POST["country"]];
+
+echo "The capital of {$country} is {$capital}.";
+
+?>
