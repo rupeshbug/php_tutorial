6ee860bc45ef7375ee95102af4db76714dc963f8
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-01-22 10:31:50 +0545

    functions

--- index.php
+++ index.php
@@ -1,31 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-</body>
-<form action="index.php" method="post">
-    <input type="radio" name="credit_card" value="Visa"> Visa <br>
-    <input type="radio" name="credit_card" value="Mastercard"> Mastercard <br>
-    <input type="radio" name="credit_card" value="American Express"> American Express <br>
-    <input type="submit" name="confirm" value="confirm">
-</form>
-
-</html>
-
 <?php
-if (isset($_POST["confirm"])) {
-    if (isset($_POST["credit_card"])) {
-        $credit_card = $_POST["credit_card"];
-        echo $credit_card;
-    } else {
-        echo "Please make a selection";
-    }
+function hypotenuse($a, $b)
+{
+    $c = sqrt($a ** 2 + $b ** 2);
+    return $c;
 }
-?>
+
+echo hypotenuse(3, 4);
