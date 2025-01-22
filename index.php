f8adb719d88ccd6f51c5dec67725fd5f5650f2d4
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-01-22 08:31:05 +0545

    radio buttons

--- index.php
+++ index.php
@@ -8,30 +8,24 @@
 </head>
 
 <body>
-    <form action="index.php" method="post">
-        <label>x: </label><br>
-        <input type=" text" name="x"><br>
-        <label>y: </label><br>
-        <input type=" text" name="y"><br>
-        <input type="submit" value="total">
-    </form>
+
 </body>
+<form action="index.php" method="post">
+    <input type="radio" name="credit_card" value="Visa"> Visa <br>
+    <input type="radio" name="credit_card" value="Mastercard"> Mastercard <br>
+    <input type="radio" name="credit_card" value="American Express"> American Express <br>
+    <input type="submit" name="confirm" value="confirm">
+</form>
 
 </html>
 
 <?php
-
-$x = $_POST["x"];
-$y = $_POST["y"];
-
-$total = null;
-
-// $total = abs($x);
-// $total = round($x);
-// $total = floor($x);
-
-$total = pow($x, $y);
-
-echo $total;
-
+if (isset($_POST["confirm"])) {
+    if (isset($_POST["credit_card"])) {
+        $credit_card = $_POST["credit_card"];
+        echo $credit_card;
+    } else {
+        echo "Please make a selection";
+    }
+}
 ?>
