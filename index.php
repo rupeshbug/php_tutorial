19bcdbb51a6eec2bb221ecbc06ab37dbb94e56b7
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-01-23 08:15:20 +0545

    other components

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include("header.html");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,36 +12,11 @@
 </head>
 
 <body>
-
+    This is the Home Page <br>
+    Stuff about your home page can go here. <br>
 </body>
-<form action="inputVal.php" method="post">
-    username: <br>
-    <input type="text" name="username"><br>
-    age: <br>
-    <input type="text" name="age"><br>
-    email: <br>
-    <input type="text" name="email"><br>
-    <input type="submit" name="login" value="login">
-</form>
 
 </html>
-
 <?php
-if (isset($_POST["login"])) {
-
-    $age = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
-    $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-
-    if (empty($age)) {
-        echo "Enter a valid number. <br>";
-    } else {
-        echo "You are $age years old. <br>";
-    }
-
-    if (empty($email)) {
-        echo "Enter a valid email. <br>";
-    } else {
-        echo "Your email is $email. <br>";
-    }
-}
+include("footer.html");
 ?>
