66d5579faac8c77e7d44c71ed2494cda4e7ba7c2
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-01-24 08:25:26 +0545

    database connection

--- database.php
+++ database.php
@@ -1,2 +1,16 @@
-<!-- mysql database connection -->
-<!-- practise -->
+<?php
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "businessdb";
+$conn = "";
+
+try {
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+} catch (mysqli_sql_exception) {
+    echo "Couldn't connect";
+}
+
+if ($conn) {
+    echo "You are connected!";
+}
