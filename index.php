72f3600f662e580271fe340cc2985a99d5a1cb6f
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-01-24 09:07:17 +0545

    data retrieval

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 include("database.php");
 
-$username = "Patrick";
-$password = "rock3";
-$hash = password_hash($password, PASSWORD_DEFAULT);
+$sql = "SELECT * FROM users";
+$result = mysqli_query($conn, $sql);
 
-$sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash')";
-
-try {
-    mysqli_query($conn, $sql);
-    echo "User is now registered! <br>";
-} catch (mysqli_sql_exception) {
-    echo "Couldn't register user! <br>";
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo $row["id"] . "<br>";
+        echo $row["user"] . "<br>";
+        echo $row["reg_date"] . "<br>";
+    }
+} else {
+    echo "User doesn't exist! <br>";
 }
 
 mysqli_close($conn);
